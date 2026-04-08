437c37d965620abca3ed35ea5f0cbb6b9a3f537e
lucaslkds <91992988+lucaslkds@users.noreply.github.com>
2026-04-07 21:27:22 -0300

    Add files via upload

---
+++ notas.cs
@@ -0,0 +1,369 @@
+/* 
+classe - ação que normalmente é um verbo
+Instancia
+Herança
+
+polimorfismo - override - sobrescrever
+@overide não é necessário, mas é bom aplicar
+encapsulamento - ignorar
+refatoralçao
+
+usar interface - Tem somente a chamada de Voo, não tem o código de como voar
+Public Interface
+Não pode ser instaciado por não ter código
+por isso se cria uma classe
+Você é obrigado a sobescrever o código da interface
+*/
+
+Estudar mais de Interface e importanção de interfaces
+Teste de pares usando IA
+padrão Extract de solução
+
+    public class Voarcomasas : Voo
+        {
+            Console.WriteLine("Voando com asas")           
+        }
+
+    public class Voarnoway : Voo
+        {
+            Console.WriteLine("Não Voar")           
+        }
+
+
+
+A gangue dos 4
+livro
+
+// https://code.visualstudio.com/docs/csharp/get-started
+
+.\aluno
+Positivo@2026
+
+Public abstract class Animal
+// abstract cant be estanciado, não entre no software somente é usado como referencia.
+Também permite definir um codigo para todos e um código abstract para ser override nas classes que precisa de algo especifico
+
+interface
+abstract
+override
+Herança
+
+Trabalho
+classe personagem
+3 subclassses
+controle de batalha
+
+
+
+Coleçõe para usar ao inves de listas e arrays
+
+
+
+Set são valores que não serão duplicados
+é possivel opitimização para quando há numeros inteiros
+e outros tipos de opitimização
+
+MAP <cahve e valor>
+melhor para busca insercão e remossão
+por ele ter a chave direto na memória
+
+threads
+usar multi threads
+look at the variable
+so two players will not be able to 
+
+API que vai enviar informações e salvar como .json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
